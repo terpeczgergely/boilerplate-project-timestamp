39857f48051f811ea2f8fbf3db6ee43d147024d8
terpeczgergely <70111786+terpeczgergely@users.noreply.github.com>
2024-01-25 22:13:11 +0100

    done

--- index.js
+++ index.js
@@ -18,10 +18,46 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+function isValidDate(input) {
+  const dateObject = new Date(input);
+
+  // Check if the dateObject is a valid date and the input is not "Invalid Date"
+  return !isNaN(dateObject.getTime()) && dateObject.toString() !== 'Invalid Date';
+}
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", (req, res) => {
+  const dateParam = req.params.date;
+
+  // Check if dateParam is not provided or empty
+  if (!dateParam) {
+    const currentDate = new Date();
+    res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+    return;
+  }
+
+  // If dateParam is a number, use it as a timestamp
+  const timestamp = Number(dateParam);
+  if (!isNaN(timestamp)) {
+    res.json({ unix: timestamp, utc: new Date(timestamp).toUTCString() });
+    return;
+  }
+
+  // If dateParam is provided as a string, attempt to create a Date object
+  const paramDate = new Date(dateParam);
+
+  // Check if the paramDate is a valid date
+  if (isValidDate(paramDate)) {
+    res.json({ unix: paramDate.getTime(), utc: paramDate.toUTCString() });
+    return;
+  }
+
+  // If dateParam is not empty but not a valid date, return an error
+  res.json({ error: 'Invalid Date' });
 });
 
 
